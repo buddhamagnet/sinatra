60dc88b82e3f9272cfe953d29f519199609bc9f3
buddhamagnet <buddhamagnet@gmail.com>
2019-11-14 10:38:52 +0000

    add tests for CodeBuild

---
+++ hellotest.rb
@@ -0,0 +1,19 @@
+ENV['APP_ENV'] = 'test'
+
+require_relative './helloworld'
+require 'test/unit'
+require 'rack/test'
+
+class HelloWorldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says_hello_world
+    get '/'
+    assert last_response.ok?
+    assert_equal 'BANG TIDY', last_response.body
+  end
+end
